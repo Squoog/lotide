ff170eed0a74d4be783c752fb919fa1776f16548
Ryan Zhen <rjhzhen@gmail.com>
2022-10-19 20:34:32 -0400

    adding findByKeyValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  let a = actual;
+  let e = expected;
+  if (actual === expected) {
+    console.log(`👌  Assertion Passed: ${a} === ${e}`);
+  } else {
+    console.log(`🤔  Assertion Failed: ${a} !== ${e}`);
+  }
+};
+
+// findByKeyValue function
+
+const findKeyByValue = function(obj, val){
+  let key;
+  let keyArray = Object.keys(obj);
+  let count = 0;
+  for (let item in obj) {
+    if (obj[item] == val){
+      key = keyArray[count];
+    }
+
+    count ++;
+  }
+  return key;
+}
+
+//Test code
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
