d230d8510c1258d2999401da91fe4727c0577605
Ryan Zhen <rjhzhen@gmail.com>
2022-10-24 18:54:03 -0400

    refactored eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,7 @@
-const assertEqual = function(actual, expected) {
-  let a = actual;
-  let e = expected;
-  if (actual === expected) {
-    console.log(`👌  Assertion Passed: ${a} === ${e}`);
-  } else {
-    console.log(`🤔  Assertion Failed: ${a} !== ${e}`);
-  }
-};
+// Retrieves assertEqual function
+const assertEqual = require('../assertEqual');
 
+// Function implementation
 const eqArrays = function(arr1, arr2) {
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
@@ -21,9 +15,5 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);  // => false
+// Exports function
+module.exports = eqArrays;
