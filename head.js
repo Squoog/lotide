3e84491933cacf1ffafa576f442cd8ff58d596ec
Ryan Zhen <rjhzhen@gmail.com>
2022-10-24 16:57:44 -0400

    Refactored haed and assertEqual functions

--- head.js
+++ head.js
@@ -1,7 +1,10 @@
+// Retrieves assertEqual function
 const assertEqual = require('./assertEqual');
 
+//Function implementation
 const head = function(arr){
   return arr[0];
 }
 
+// Exports function
 module.exports = head;
