ed9a5de459fc980d6b37f5009eb1bc8803c02f10
Ryan Zhen <rjhzhen@gmail.com>
2022-10-24 16:55:14 -0400

    Refactored haed and assertEqual functions

--- assertEqual.js
+++ assertEqual.js
@@ -9,9 +9,5 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("French Horn","French Horn");
-assertEqual(1, 1);
-assertEqual(2, 'two');
-assertEqual(3, 4);
+// Exports function
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,19 +1,7 @@
-const assertEqual = function(actual, expected) {
-  let a = actual;
-  let e = expected;
-  if (actual === expected) {
-    console.log(`👌  Assertion Passed: ${a} === ${e}`);
-  } else {
-    console.log(`🤔  Assertion Failed: ${a} !== ${e}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arr){
   return arr[0];
 }
 
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5,6,7]), 7);
-assertEqual(head([]), 1);
+module.exports = head;
